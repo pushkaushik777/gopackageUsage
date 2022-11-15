af1f0e053609295976a9962e3fc1e81156e7b4ed
piyush <pushkaushik777@gmail.com>
2022-11-16 02:15:47 +0530

    Adding Slice program

--- channla/main.go
+++ channla/main.go
@@ -16,4 +16,15 @@ func main() {
 	fmt.Println(p.Name)
 	fmt.Println(p.Company)
 	fmt.Println(p.Salary)
+
+	//Testing Slices & Array
+	var arr [7]int = [7]int{12, 33, 34, 5, 2, 3, 2}
+	fmt.Println(arr)
+	fmt.Println(len(arr))
+	fmt.Println(cap(arr))
+	var s []int = []int{3: 3}
+	fmt.Println(s)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+
 }
